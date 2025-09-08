4a704ddf71de221205e7a0df1f6cb40d6c11b997
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2025-09-08 17:56:27 +0600

    add Geometry class with static fields, properties, and calculation methods

--- StringBuilder/Program.cs
+++ StringBuilder/Program.cs
@@ -1,6 +1,6 @@
 using System.Text;
 
-StringBuilder s = new StringBuilder("I am Naeem ");
+StringBuilder s = new StringBuilder("I am Shahrukh ");
 s= s.Append("and Student of Comilla University");
 s= s.AppendLine(" and CSE Deopartment");
 // Insert,Remove,Replace 
